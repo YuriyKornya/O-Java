f810f373bb7c82c738c844cf04be0ade7a984c2e
YuriyKornya <yuriy_kornya@icloud.com>
2025-06-12 02:20:02 +0300

    Lab5, Lab12 (main)

--- src/Lab12/task3/ElectronicsProduct.java
+++ src/Lab12/task3/ElectronicsProduct.java
@@ -3,11 +3,13 @@ package Lab12.task3;
 public class ElectronicsProduct extends Product{
     private String brand;
 
+    public ElectronicsProduct(String name, int price, int id, String brand) {
         super(name, price, id);
         this.brand = brand;
     }
 
     @Override
     public String getDescription () {
+        return ("Електроніка: " + name + " Ціна: " + price + " Бренд: " + brand);
     }
 }
--- src/Lab12/task3/FoodProduct.java
+++ src/Lab12/task3/FoodProduct.java
@@ -1,12 +1,15 @@
 package Lab12.task3;
 
+public class FoodProduct extends Product implements Discountable{
     private String expirationDate;
 
+    public FoodProduct (String name, int price, int id, String expirationDate) {
         super(name, price, id);
         this.expirationDate = expirationDate;
     }
 
     public String getDescription () {
+        return ("Продукт: " + name + " Ціна: " + price + " грн" + "Придатний до: " + expirationDate);
     }
 
     public void applyDiscount (double percent) {
